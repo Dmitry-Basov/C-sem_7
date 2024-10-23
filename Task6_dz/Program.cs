3d83377c997b1feb2dcbe495b5430bbd6f48aac2
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-24 00:31:52 +0300

    Task6

---
+++ Task6_dz/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу для вычисления функции Аккермана с помощью
+// рекурсии. Даны два неотрицательных числа m и n.
+// Пример:
+// ● Вход: m = 2, n = 3
+// ● Выход: A(m, n) = 29
+// ● Вход: m = 1, n = 4
+// ● Выход: A(m, n) = 7
+
+
+
+int Ancermanfunction(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    else if (m == 0)
+    {
+        return Ancermanfunction(n - 1, 1);
+    }
+        return Ancermanfunction(n - 1, Ancermanfunction(n, m - 1));
+    
+}
+
+int m = 1;
+int n = 4;
+
+int result = Ancermanfunction(n, m);
+Console.WriteLine($"A({m}, {n}) = {result} ");
