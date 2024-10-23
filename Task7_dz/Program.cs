969f97687c0f2bbe8fb13055fd6d79d722f615d1
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-24 01:17:08 +0300

    Task7

---
+++ Task7_dz/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте произвольный массив. Выведите его элементы, начиная с конца.
+// Используйте рекурсию и не используйте циклы.
+
+
+
+void PrintArrayReverse(int[] array, int index)
+{
+    if (index < 0) return;
+    {
+        Console.Write(array[index]);
+    }
+    if (index > 0)
+    {
+        Console.Write(", ");
+        PrintArrayReverse(array,index -1);
+    }
+}
+
+int[] arr = {1,2,3,4,5};
+Console.WriteLine("Массив в обратном порядке");
+PrintArrayReverse(arr, arr.Length - 1);
