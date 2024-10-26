cb1f9c711b8ff7b28de346a3eb6e85548d80a09e
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-27 00:57:43 +0300

    Task7 dz 2.0

--- Task7_dz/Program.cs
+++ Task7_dz/Program.cs
@@ -3,19 +3,19 @@
 
 
 
-void PrintArrayReverse(int[] array, int index)
+void PrintArrReverse(int[] array, int index)
 {
+    Console.Write(array[index]);
+    
     if (index < 0) return;
-    {
-        Console.Write(array[index]);
-    }
+
     if (index > 0)
     {
         Console.Write(", ");
-        PrintArrayReverse(array,index -1);
+        PrintArrReverse(array, index - 1);
     }
 }
 
-int[] arr = {1,2,3,4,5};
+int[] arr = { 1, 9, 3, 4, 5 };
 Console.WriteLine("Массив в обратном порядке");
-PrintArrayReverse(arr, arr.Length - 1);
+PrintArrReverse(arr, arr.Length - 1);
